b3713446978bd06771d13cc50cf3542e06bc460d
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2025-01-07 10:03:08 +0530

    return email by id

--- ExMart_Backend/Model/Banner.cs
+++ ExMart_Backend/Model/Banner.cs
@@ -11,6 +11,7 @@ namespace ExMart_Backend.Model
 
         public int BannerId { get; set; }
         public string ImageUrl { get; set; }
+        public string ProductName { get; set; }
         [ForeignKey("ProductId")]
 
         public int ProductId { get; set; }
--- ExMart_Backend/Model/OrderItem.cs
+++ ExMart_Backend/Model/OrderItem.cs
@@ -1,7 +1,12 @@
-﻿namespace ExMart_Backend.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExMart_Backend.Model
 {
     public class OrderItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int OrderItemId { get; set; }
 
         public int ProductId { get; set; }
