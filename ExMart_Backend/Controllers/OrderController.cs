e998bfd366bce7beff3a433a9cff87a42de37dd9
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2025-01-07 16:28:33 +0530

    update status by id added a slash

--- ExMart_Backend/Controllers/OrderController.cs
+++ ExMart_Backend/Controllers/OrderController.cs
@@ -158,7 +158,7 @@ namespace ExMart_Backend.Controllers
                 }
             }
 
-        [HttpPut("updatestatusbyidonly{orderitemid}")]
+        [HttpPut("updatestatusbyidonly/{orderitemid}")]
         public async Task<IActionResult> UpdateOrderItemStatusByIdOnly(int orderitemid)
         {
             try
