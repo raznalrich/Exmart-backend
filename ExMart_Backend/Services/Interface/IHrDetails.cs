0d6a40787ff694e7ebb8ba35f406535c9d292701
Chriswin Lingson <chriswin.lingson@experionglobal.local>
2025-01-23 14:43:33 +0530

    Hr details adding v1 23.1.25

---
+++ ExMart_Backend/Services/Interface/IHrDetails.cs
@@ -0,0 +1,11 @@
+﻿using ExMart_Backend.Model;
+
+namespace ExMart_Backend.Services.Interface
+{
+    public interface IHrDetails
+    {
+        //Task<IEnumerable<HrDetails>>GetDetailsRepo(int HrDid);
+        Task<HrDetails> GetDetailsRepo(int HrDid);
+        Task<HrDetails> EditDetailsRepo(HrDetails hrDetails);
+    }
+}
